7815c3fdb5eb0f64cc3e4037cd1950887585c416
suvadityagupta <10suvaditya18@gmail.com>
2024-05-06 13:06:12 +0530

    "Added functionality to fetch and display news articles on load; added functions to bind fetched data to the DOM; updated index.html to include an id for cards-container"

--- News App/script.js
+++ News App/script.js
@@ -1,2 +1,25 @@
 const API_KEY ="71815003bdca4f38be63015841c182f4";
 const url = "https://newsapi.org/v2/everything?q=";
+window.addEventListner('Load' ,() => fetchNews("India") );
+async function fetchNews(query){
+    const res = await fetch(`${url}${query}&apiKEY=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+function bindData(articles){
+    const cardsContainer = document.getElementById('cards-container')
+    const newsCardTemplate = document.getElementById('template-news-card')
+    cardsContainer.innerHTML = "";
+    articles.forEach(article => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);    
+    });
+}
+function fillDataInCard(cardClone , article){
+    const newsImg = cardClone.querySelectior('#news-img');
+    const newsTitle = cardClone.querySelectior('#news-title');
+    const newsSource = cardClone.querySelectior('#news-source');
+    const newsDesc = cardClone.querySelectior('#news-desc');
+}
